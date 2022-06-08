61d90dc1253fd9209e141569f2f69bbfa357d1c8
Andrew Cardoso <andrewgcardoso@gmail.com>
2022-06-08 00:17:17 -0300

    fix - remove console.log in home

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -4,7 +4,6 @@ import {PostsContainer} from './PostsContainer';
 import {Post} from '../../_api/models/posts';
 import {usePostsApi} from '../../_api/api.hook';
 import {Spinner} from '../Spinner';
-import {Temporal} from '@js-temporal/polyfill';
 
 const AsideNav = lazy(() => import('./Aside'));
 const HiddenNav = lazy(() => import('./HiddenNav'));
@@ -13,8 +12,6 @@ export const Home = () => {
 	const api = usePostsApi();
 	const [posts, setPosts] = useState<Post[]>([]);
 	const [screenWidth] = useState(window.screen.availWidth);
-
-	console.log(Temporal);
 
 	useEffect(() => {
 		api.getPosts().then((data) => setPosts(data));
