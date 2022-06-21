200e9142b660d6065dd6bf4fda4e6f171a4397fb
Andrew Cardoso <andrewgcardoso@gmail.com>
2022-06-20 23:59:29 -0300

    another test

--- src/components/Admin/Users/index.tsx
+++ src/components/Admin/Users/index.tsx
@@ -31,7 +31,7 @@ const UserCells = ({
 		<TableCell>{email}</TableCell>
 		<TableCell>{SignInTypeView[signInType]}</TableCell>
 		<TableCell>
-			<RolesContainer>
+			{/* <RolesContainer>
 				{ROLES.map((role) => (
 					<Switch
 						key={role + index}
@@ -40,7 +40,7 @@ const UserCells = ({
 						label={getRolesView(role)}
 					/>
 				))}
-			</RolesContainer>
+			</RolesContainer> */}
 		</TableCell>
 	</>
 );
